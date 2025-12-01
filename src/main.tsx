33141badb70061d48c3c9cfe7843630242c6834a
Sviatoslav <sviat.danko@gmail.com>
2025-12-01 14:19:43 +0000

    change main - QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Нормалізація стилів
-import 'modern-normalize/modern-normalize.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "modern-normalize/modern-normalize.css";
 import App from "./components/App/App";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
